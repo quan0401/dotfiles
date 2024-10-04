d10e551275971e107f375c5b256b7b7ce5701bf7
quan0401 <dongminhquan2004@gmail.com>
2024-10-04 21:44:33 +0700

    nnn with ;e to edit in vim, nvimtree <leader>ov to open in new iterm window tmux nvim

--- nnn/.config/nnn/plugins/open-with-nvim.sh
+++ nnn/.config/nnn/plugins/open-with-nvim.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Check if a file is provided
+# Check if a file or directory is provided
 if [ -z "$1" ]; then
-  # No file is provided, open the current directory in nvim
-  nvim "$(pwd)"
+  # No file is provided, open the current directory in a new tmux vertical pane with nvim
+  tmux split-window -h "nvim $(pwd)"  # -h creates a vertical split in tmux
 else 
-  # If a file is provided, copy the absolute path to the clipboard and open it in nvim
-  path="$(pwd)/$1"
-  nvim "$path"
+  # If a file is provided, copy the absolute path to the clipboard and open it in a new tmux vertical pane with nvim
+  path="$(realpath "$1")"  # Get the absolute path
+  tmux split-window -h "nvim $path"  # Open the file in a new tmux vertical pane
 fi
 
--- nvim/.config/nvim/lua/quan0401/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/quan0401/plugins/nvim-tree.lua
@@ -8,12 +8,55 @@ return {
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 
+		-- Function to open iTerm2, start tmux, and open nvim in the selected directory
+		local function open_iterm_tmux_nvim(node)
+			local path = node.absolute_path
+			-- Use AppleScript to open a new iTerm2 window and execute tmux and nvim
+			-- local command = 'osascript -e \'tell application "iTerm"\n    create window with default profile\n    tell current session of current window\n        write text "tmux new-session; send-keys \\"cd '
+			-- 	.. path
+			-- 	.. ' && nvim\\" C-m"\n    end tell\nend tell\''
+			local command = 'osascript -e \'tell application "iTerm"\n'
+				.. "    create window with default profile\n"
+				.. "    delay 0.5\n" -- Introduce a 0.5-second delay to ensure the window is ready
+				.. "    tell current session of current window\n"
+				.. '        write text "tmux new-session; send-keys \\"cd '
+				.. path
+				.. ' && nvim\\" C-m"\n'
+				.. "    end tell\n"
+				.. "end tell'"
+			os.execute(command)
+		end
+
+		local function my_on_attach(bufnr)
+			local api = require("nvim-tree.api")
+
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+
+			vim.keymap.set("n", "ss", function()
+				local node = require("nvim-tree.lib").get_node_at_cursor()
+				if node then
+					open_iterm_tmux_nvim(node)
+				end
+			end, opts("Open in iTerm2, start tmux, and open nvim in the selected directory"))
+
+			-- default mappings
+			api.config.mappings.default_on_attach(bufnr)
+
+			-- custom mappings
+			vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up"))
+			vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+		end
+
 		nvimtree.setup({
 			view = {
 				width = 35,
 				relativenumber = true,
 				side = "right",
 			},
+			-- on_attach = my_on_attach,
+
 			-- change folder arrow icons
 			renderer = {
 				indent_markers = {
@@ -62,6 +105,13 @@ return {
 				os.execute("nvim " .. node.absolute_path .. " &") -- Use & to run it in the background
 			end
 		end
+
+		keymap.set("n", "<leader>ov", function()
+			local node = require("nvim-tree.lib").get_node_at_cursor()
+			if node then
+				open_iterm_tmux_nvim(node)
+			end
+		end, { desc = "Open in iTerm2, start tmux, and open nvim in the selected directory" })
 
 		keymap.set("n", "<leader>ot", open_in_new_terminal, { desc = "Open folder in new terminal window" })
 	end,
